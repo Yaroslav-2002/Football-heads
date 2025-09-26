e5f55ec5a813df22637d85c6cca19b89a8a9ed2c
Yaroslav-2002 <81532518+Yaroslav-2002@users.noreply.github.com>
2025-09-26 23:36:09 +0300

    Setup initial FootballHeads project structure

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
